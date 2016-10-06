8f372ef0f2637e371df80a9cf31cc68c65c182ae
Peter Elbaum <elbaumpj@gmail.com>
2016-10-06 13:24:25 -0400

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,13 @@
+alert("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikey.js');
+
+if(githubtoken !== undefined){
+ $.ajaxSetup({
+   headers: {
+     'Authorization': 'token ' + githubtoken.token
+   }
+ });
+}
