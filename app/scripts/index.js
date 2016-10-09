053f0aad47cf02707ad5dcda42300deba048da70
Peter Elbaum <elbaumpj@gmail.com>
2016-10-09 18:26:53 -0400

    commented out token

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,13 +3,13 @@ var _ = require('underscore');
 var Handlebars = require('handlebars');
 var githubtoken = require('./gitapikey.js');
 
-if(githubtoken !== undefined){
- $.ajaxSetup({
-   headers: {
-     'Authorization': 'token ' + githubtoken.token
-   }
- });
-}
+// if(githubtoken !== undefined){
+//  $.ajaxSetup({
+//    headers: {
+//      'Authorization': 'token ' + githubtoken.token
+//    }
+//  });
+// }
 
 
 $.ajax('https://api.github.com/users/elbaumpj').then(getUserInfo);
